9381bfbb93227763b99bf90320463c25283b3215
Harry <harikrishnasowreddy14@gmail.com>
2025-03-17 14:59:20 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import Usercard from './components/usercard.jsx';
+import Usercard from './components/Usercard.jsx';
 
 function App() {
   const cards = Array.from({ length: 10 });
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
-// write the component code here
+import "./usercard.css";
+
+const Usercard = () => {
+  const profilePhoto = "https://th.bing.com/th/id/OIP.ZugpBU6RwS8ftzBQyaXuegHaJQ?w=208&h=260&c=7&r=0&o=5&dpr=1.3&pid=1.7"; // 
+  const name = "Harry";
+  const email = "hari.doe@example.com";
+  const phone = "+91825061";
+  const address = "143 side Street, Graveyard, UK";
+
+  return (
+    <div className="usercard">
+      <img src={profilePhoto} alt="Profile" className="profile-photo" />
+      <div className="user-details">
+        <h2>{name}</h2>
+        <p>Email: {email}</p>
+        <p>Phone: {phone}</p>
+        <p>Address: {address}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Usercard;
